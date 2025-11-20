5b27432c34dd80c67fa192faab92c2babbcf26c4
aniket-g-3101 <aniket.g.dev@gmail.com>
2025-11-20 15:53:39 +0530

    Second Question Solved

---
+++ Practice Before Dsa/Q2.java
@@ -0,0 +1,31 @@
+
+//2.Find the maximum element in an array
+
+import java.util.Scanner;
+
+public class Q2 {
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Elements Of Array : ");
+        for (int i = 0; i < arr.length; i++) {
+
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array is :");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        int max = arr[0]; // Start with first element
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("\nMaximum Element of Array is : " + max);
+        sc.close();
+    }
+}
