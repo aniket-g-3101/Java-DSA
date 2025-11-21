89e13770ac687ef4d488f5e5f79406c3902a683b
aniket-g-3101 <aniket.g.dev@gmail.com>
2025-11-21 15:26:21 +0530

    All Array Questions Solved

--- Practice Before Dsa/ArrayQ_10.java
+++ Practice Before Dsa/ArrayQ_10.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class ArrayQ10 {
+public class ArrayQ_10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int arr[] = new int[5];
