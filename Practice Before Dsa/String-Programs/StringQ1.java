f718847cf825887bbcc38a8dcefd79c2bb84bd29
aniket-g-3101 <aniket.g.dev@gmail.com>
2025-11-23 16:04:24 +0530

    Second Solved

--- Practice Before Dsa/String-Programs/StringQ1.java
+++ Practice Before Dsa/String-Programs/StringQ1.java
@@ -9,6 +9,15 @@ public class StringQ1 {
         String palindrome = sc.next();
 
         StringBuilder sb = new StringBuilder(palindrome);
+        // `String reverse = sb.reverse().toString();` is reversing the input string
+        // `palindrome` using
+        // the `reverse()` method of the `StringBuilder` class. The `reverse()` method
+        // reverses the
+        // characters in the `StringBuilder` object `sb`. Then, `toString()` method is
+        // called to
+        // convert the reversed `StringBuilder` object back to a `String` and store it
+        // in the variable
+        // `reverse`.
         String reverse = sb.reverse().toString();
 
         if (palindrome.equals(reverse)) {
---
+++ Practice Before Dsa/String-Programs/StringQ2.java
@@ -0,0 +1,26 @@
+// 2.Count vowels and consonants
+
+import java.util.Scanner;
+
+public class StringQ2 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String : ");
+        String str = sc.nextLine();
+        System.out.println("Original length of String : " + str.length());
+        int len = str.length();
+        int vowels = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o'
+                    || str.charAt(i) == 'u' || str.charAt(i) == 'A' || str.charAt(i) == 'E' || str.charAt(i) == 'I'
+                    || str.charAt(i) == 'O' || str.charAt(i) == 'U') {
+                vowels += 1;
+                len -= 1;
+            }
+        }
+        System.out.println("Count of Vowels in String : " + vowels);
+        System.out.println("Count of Consonants in String : " + len);
+        sc.close();
+
+    }
+}
