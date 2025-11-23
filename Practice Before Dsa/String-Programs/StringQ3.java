3ea353090c174b90852fcc6631fefd8e055efa38
aniket-g-3101 <aniket.g.dev@gmail.com>
2025-11-23 16:14:48 +0530

    Third solved

---
+++ Practice Before Dsa/String-Programs/StringQ3.java
@@ -0,0 +1,18 @@
+// 3.Reverse a string
+
+import java.util.Scanner;
+
+public class StringQ3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Any String : ");
+        String str = sc.nextLine();
+        System.out.println("Original String : " + str);
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+        System.out.println("Reversed String : " + rev);
+        sc.close();
+    }
+}
