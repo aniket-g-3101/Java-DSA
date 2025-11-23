77c2a280ad4a82f3d1ec35f10c568184a532f4e1
aniket-g-3101 <aniket.g.dev@gmail.com>
2025-11-23 14:14:48 +0530

    StringBuilder Class

--- Practice Before Dsa/String-Programs/StringBuilders.java
+++ Practice Before Dsa/String-Programs/StringBuilders.java
@@ -1,3 +1,11 @@
+// A StringBuilder in Java is a class used to create and modify strings without creating new string objects every time.
+
+// Common methods:
+// append() – add text
+// insert() – insert text at a position
+// delete() – remove text
+// reverse() – reverse the entire string
+// toString() – convert it back to a normal String
 
 public class StringBuilders {
     public static void main(String[] args) {
