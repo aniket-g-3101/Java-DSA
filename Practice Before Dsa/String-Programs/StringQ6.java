5db20ae78b6cc3c9ce95ed25e6248d43f635fffe
aniket-g-3101 <aniket.g.dev@gmail.com>
2025-11-23 17:39:54 +0530

    Sixth Solved

---
+++ Practice Before Dsa/String-Programs/StringQ6.java
@@ -0,0 +1,28 @@
+//6.Find the frequency of each character
+
+import java.util.Scanner;
+
+public class StringQ6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Any String : ");
+        String str = sc.nextLine();
+        System.out.println("Entered String : " + str);
+
+        int freq[] = new int[256]; // covers all ASCII characters
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            freq[ch]++;
+        }
+
+        System.out.println("Character frequency:");
+        for (int i = 0; i < 256; i++) {
+            if (freq[i] > 0) {
+                System.out.println((char) i + " = " + freq[i]);
+            }
+        }
+        sc.close();
+    }
+
+}
