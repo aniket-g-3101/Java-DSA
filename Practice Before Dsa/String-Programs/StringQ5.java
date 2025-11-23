67b1f12eff5c1d20b2e084cba3642eaba67cd941
aniket-g-3101 <aniket.g.dev@gmail.com>
2025-11-23 17:27:50 +0530

    Fifth Solved

---
+++ Practice Before Dsa/String-Programs/StringQ5.java
@@ -0,0 +1,16 @@
+// 5.Convert lowercase to uppercase
+
+import java.util.Scanner;
+
+public class StringQ5 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Any String : ");
+        String str = sc.nextLine();
+        System.out.println("Entered String : " + str);
+        String result = str.toUpperCase();
+        System.out.println("Updated String : " + result);
+        sc.close();
+
+    }
+}
