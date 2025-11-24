5e5c459993810c2adbe2cded0e13513b10ce62fa
aniket-g-3101 <aniket.g.dev@gmail.com>
2025-11-24 13:32:20 +0530

    Second solved

---
+++ Practice Before Dsa/Function-Programs/FunctionQ2.java
@@ -0,0 +1,26 @@
+// 2.Write a function to generate Fibonacci up to n terms
+
+import java.util.Scanner;
+
+public class FunctionQ2 {
+
+    public static void Program(int n) {
+        int a = 0, b = 1;
+        System.out.print(a + " " + b + " ");
+        for (int i = 2; i <= n; i++) {
+            int c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Any Number : ");
+        int num = sc.nextInt();
+        Program(num);
+        sc.close();
+    }
+}
