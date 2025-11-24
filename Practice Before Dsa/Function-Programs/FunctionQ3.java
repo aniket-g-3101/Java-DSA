1af66db2dde9e839cad691706a5b6b8b0b795d2e
aniket-g-3101 <aniket.g.dev@gmail.com>
2025-11-24 13:36:04 +0530

    Third solved

---
+++ Practice Before Dsa/Function-Programs/FunctionQ3.java
@@ -0,0 +1,35 @@
+// 3.Write a function to check if a number is prime
+
+import java.util.Scanner;
+
+public class FunctionQ3 {
+
+    public static void IsPrime(int n) {
+        boolean isPrime = true;
+
+        if (n <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(n + " is a prime number");
+        } else {
+            System.out.println(n + " is not a prime number");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Any Number : ");
+        int num = sc.nextInt();
+        IsPrime(num);
+        sc.close();
+    }
+}
