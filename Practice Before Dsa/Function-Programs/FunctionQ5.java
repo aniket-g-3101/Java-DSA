4716b8030d17a7006314e69cb28051f3a46b16ff
aniket-g-3101 <aniket.g.dev@gmail.com>
2025-11-24 13:47:34 +0530

    Fifth solved

---
+++ Practice Before Dsa/Function-Programs/FunctionQ5.java
@@ -0,0 +1,24 @@
+// 5.Write a function to return the sum of digits of a number
+
+import java.util.Scanner;
+
+public class FunctionQ5 {
+    public static int DigitSum(int n) {
+        int sum = 0;
+        while (n != 0) {
+            int r = n % 10;
+            sum += r;
+            n = n / 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Any Number : ");
+        int num = sc.nextInt();
+        int result = DigitSum(num);
+        System.out.println("Sum of Digits : " + result);
+        sc.close();
+    }
+}
